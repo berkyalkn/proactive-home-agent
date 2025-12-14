f404dab6d0d0bacf8e4a8c4736d5e794ef925e37
Berkay Alkan <berky.alknn@gmail.com>
2025-12-14 13:20:10 +0000

    fix(backend) Bugs fixed

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,8 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://100.105.136.5:3000",
-    "http://raspberrypi.local:3000"
+    "http://raspberrypi.local:3000",
+    "http://192.168.1.149:3000"
 ]
 
 app.add_middleware(
